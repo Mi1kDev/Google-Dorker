0c727d164823dc999b45ba1e4537a4b34fa376f6
Mi1kDev <mi1kgamedev@gmail.com>
2020-11-06 21:32:40 +0000

    Updated comments for the dorker class as well as an example program to demonstrate use.

--- Classes/dorker.py
+++ Classes/dorker.py
@@ -2,15 +2,18 @@ import urllib.parse
 import requests
 import re
 import os
+import datetime
 from bs4 import BeautifulSoup
 
 class Dorker:
     def __init__(self, query, num, **kwaargs):
-        self.host = "https://www.google.com"
-        self.baseUrl = "https://www.google.com/search?"
+        #initializes the class with its base attributes
+        self.host = "https://www.google.com" #this is the url appended to the front of found links
+        self.baseUrl = "https://www.google.com/search?" #this is the base search url from which the final url will extend
         self.query = query
         self.num = num
-        self.finalUrl = None
+        self.finalUrl = None #the final url is set to None at first as we do not yet know what value it will be
+        #these are all the optional paramters that can be used to help narrow down search results
         self.params = {
             "as_eqp": "Results must include this query in the same word order",
             "as_oq": "Results must include one or more words in this string.",
@@ -21,8 +24,19 @@ class Dorker:
             "as_lq": "Finds sites that link to a given url",
         }
         self.foundUrls = []
+        #creates the final url from which we will scrape links from during the initialization
         self.formatUrl(**kwaargs)
+    
+    #this function simply logs any errors found during the runtime of the program using this class
+    def logError(self, errorMsg):
+        if not os.path.isfile("Errors.log"):
+            with open("Errors.log", "w") as e:
+                e.write("[!] At {t} an error occurred. The error message is as follows.\n{e}\n".format(t=datetime.datetime.now(), e=errorMsg))
+        else:
+            with open("Errors.log", "a") as e:
+                e.write("[!] At {t} an error occurred. The error message is as follows.\n{e}\n".format(t=datetime.datetime.now(), e=errorMsg))
 
+    #this function will format the search url based on the given optional paramters and their values
     def formatUrl(self, **kwaargs):
         params = {"q": self.query, "num": self.num}
         for key, value in kwaargs.items():
@@ -31,28 +45,41 @@ class Dorker:
         params = urllib.parse.urlencode(params)
         self.finalUrl = self.baseUrl + params
     
+    #this function will simply list all the paramters and what they do
     def paramList(self):
         for key, value in self.params.items():
             print("[+] {k}: {v}".format(k=key, v=value))
     
     def findResults(self):
-        r = requests.get(self.finalUrl)
-        soup = BeautifulSoup(r.content, "html.parser")
-        results = []
-        for link in soup.find_all("a", href=True):
-            if not "http" in link["href"][0:4] and "search?q={q}".format(q=self.query.replace(" ", "+")) in link["href"]:
-                results.append(self.host + link["href"])
-        
-        self.foundUrls = results
+        try:
+            #makes a request to the properly formatted url
+            r = requests.get(self.finalUrl)
+            #creates a BeautifulSoup object from the results
+            soup = BeautifulSoup(r.content, "html.parser")
+            results = []
+            #finds all urls on the page
+            for link in soup.find_all("a", href=True):
+                #limits the urls that are appended to meaningful ones
+                if not "http" in link["href"][0:4] and "search?q={q}".format(q=self.query.replace(" ", "+")) in link["href"]:
+                    results.append(self.host + link["href"])
+            
+            self.foundUrls = results
+        except Exception as e:
+            self.logError(e)
     
     def exportToTextFile(self):
         if len(self.foundUrls) <= 0:
             print("[!] Either no urls were found, or you have not run the find results function")
             return None
         else:
-            self.createFolder("Text Files")
-            with open(os.path.join("Text Files", self.query+".txt"), "w") as q:
-                q.write("\n".join(self.foundUrls) + "\n")
+            try:
+                #creates a folder to save the text files if there is none
+                self.createFolder("Text Files")
+                #writes the found urls into the text file line by lines
+                with open(os.path.join("Text Files", self.query+".txt"), "w") as q:
+                    q.write("\n".join(self.foundUrls) + "\n")
+            except Exception as e:
+                self.logError(e)
     
     def createFolder(self, name):
         if not os.path.isdir(name):
@@ -63,19 +90,23 @@ class Dorker:
             print("[!] Either no urls were found, or you have not run the find results function")
             return None
         else:
-            i = 0
-            for link in self.foundUrls:
-                r = requests.get(link)
-                if "content-disposition" in r.headers:  
-                    headers = r.headers["content-disposition"]
-                    filename = re.findall("filename=(.+)", headers)[0]
-                    if filename is None:
+            try:
+                i = 0
+                #downloads from each link that was found and stores the data in a file
+                for link in self.foundUrls:
+                    r = requests.get(link)
+                    if "content-disposition" in r.headers:  
+                        headers = r.headers["content-disposition"]
+                        filename = re.findall("filename=(.+)", headers)[0]
+                        if filename is None:
+                            filename = str(i)
+                            i += 1
+                    else:
                         filename = str(i)
                         i += 1
-                else:
-                    filename = str(i)
-                    i += 1
-                dirName = self.query+" Files"
-                self.createFolder(dirName)
-                with open(os.path.join(dirName, filename), "wb") as f:
-                    f.write(r.content)
+                    dirName = self.query+" Files"
+                    self.createFolder(dirName)
+                    with open(os.path.join(dirName, filename), "wb") as f:
+                        f.write(r.content)
+            except Exception as e:
+                self.logError(e)
---
+++ Example.py
@@ -0,0 +1,12 @@
+from Classes import Dorker
+query = "passwords" #any main search query to act as a basis for the search
+num = 10 #any integer up to 100
+dorker = Dorker(
+    query,
+    num, 
+    as_filetype="log" #optional search parameters
+    )
+#dorker.paramList() this will show each optional search parameter and what it does
+dorker.findResults() #finds any relevant search results
+dorker.exportToTextFile() #exports links to a text file
+#dorker.saveLinks() this will download the content from each url found
